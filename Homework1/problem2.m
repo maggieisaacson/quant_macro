11293225e8990e12cd4a85d1ed4c8bec1f5b7f4e
maggieisaacson <misaacson42@gmail.com>
2024-10-15 16:49:11 -0400

    problem 3 gradient descents half done in spite of me being the dumbest girl in the world (forgetting T means transpose)

--- Homework1/problem2.m
+++ Homework1/problem2.m
@@ -20,6 +20,8 @@ n = 1000 ;
 a = 0 ; 
 b = T ; 
 h = (b - a)/n ; 
+
+tic
 midpoint_integral = 0 ; 
 for j = 1:n
     a_j = a + (j-1)*h;
@@ -28,17 +30,20 @@ for j = 1:n
     % Midpoint integral (sum) 
     midpoint_integral = midpoint_integral + (b_j-a_j)*f(c_j,rho,lambda) ;
 end 
+toc
 
 % Trapezoid integral 
-
+tic
 trapezoid_integral = 0 ; 
 for j = 1:n-1 
     a_j = a + (j-1)*h;
     b_j = a + (j)*h;
     trapezoid_integral = trapezoid_integral + (b_j-a_j)/2*(f(a_j,rho,lambda) + f(b_j,rho,lambda)) ; 
 end 
+toc
 
-% Simpson's Rule 
+% Simpson's Rule
+tic
 simpsons_integral = 0 ; 
 for j = 1:n 
     a_j = a + (j-1)*h;
@@ -46,14 +51,16 @@ for j = 1:n
     c_j = (a_j + b_j)/2 ; 
     simpsons_integral = simpsons_integral + (b_j - a_j)/6*(f(a_j, rho, lambda) + 4*f(c_j,rho,lambda) + f(b_j, rho, lambda)); 
 end 
+toc
 
 %% 3. Monte Carlo 
+tic
 rng(926) ;
 mc_integral = 0 ; 
 X = 100*rand(n,1) ;
 f_X = arrayfun(@f,X,rho*ones(n,1),lambda*ones(n,1)) ;
 mc_integral = mean(f_X, "all") ;
- 
+toc 
 
 %% 4. Comparison 
 
--- Homework1/problem3.m
+++ Homework1/problem3.m
@@ -4,13 +4,101 @@
 %  min_{x,y} 100(y - x^2)^2 + (1 - x)^2 
 
 %% 0. Housekeeping 
+clear variables 
+close all 
 
-%% 1. Newton-Raphson 
+% For consistency across methods, maximum iterations allowed will be the
+% same and set here 
+maxit = 10000 ; 
 
-%% 2. BFGS 
+%% 1. Steepest Descent 
 
-%% 3. Steepest Descent 
+% Start with an initial guess of x and y 
+% After looking at a 3D graph of the function, the minimum looks like it's
+% around (1,1) so that's where I'll start
+tol = 10^-8 ; 
+alpha = 0.0001; 
 
-%% 4. Conjugate Descent 
+% Ok so it is possibly exactly (1,1) so let's start somewhere else 
+x_y_sd = [0.5, 1.5] ;  
+n = 100 ; 
+it = 0 ; 
+
+tic
+% Check termination condition 
+% Is the gradient very close to 0? 
+while n > tol && it < maxit
+    gradient = grad(x_y_sd) ; 
+    n = norm(gradient) ;  
+    
+    % Determine descent direction 
+    % d = - gradient(f(x_k))/norm(gradient(f(x_k))) 
+    d = -gradient./n ; 
+    
+    % Step size 
+    % Probably just do a small deterministic step size? (alpha set above) 
+    
+    % Next point and start over 
+    % New point 
+    x_y_sd = x_y_sd + alpha.*d ; 
+    it = it + 1 ; 
+end 
+
+toc 
+
+%% 2. Conjugate Descent  
+
+% Initial Guess 
+x_y_cd = [0.5, 1.5] ;  
+
+% Initialize the loop 
+n = 100 ; 
+it = 0 ; 
+beta = 0 ;
+d = 0 ;
+
+tic
+while n > tol && it < maxit
+    % Condition for stopping 
+    n = norm(gradient) ; 
+     
+    % Calculate new direction 
+    g= grad(x_y_cd) ; 
+    dplus = -g + beta*d;
+    
+    % Create new guess 
+    x_y_cd = x_y_cd + alpha.*dplus ; 
+    it = it + 1 ; 
+    
+    % New gradient for beta 
+    gplus = grad(x_y_cd) ; 
+
+    % Save old d for next loop 
+    d = dplus ; 
+
+    % Update beta for next loop 
+    beta = (gplus*transpose(gplus))/(g*transpose(g)) ;
+end
+
+toc 
+
+%% 3. Newton-Raphson 
+
+
+%% 4. BFGS 
+
 
 %% Function Appendix 
+function z = f(pair)
+    x = pair(1) ;
+    y = pair(2) ; 
+    z = 100*(y -x^2)^2 + (1-x)^2 ; 
+end 
+
+function z = grad(pair)
+    x = pair(1) ;
+    y = pair(2) ; 
+    z1 = 200*(y - x^2)*(-2*x) - 2*(1-x) ;
+    z2 = 200*(y-x^2) ;
+    z = [z1,z2] ;
+end 
