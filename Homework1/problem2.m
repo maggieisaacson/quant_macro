a4b745b73e3c45cd07bfc9be22ecf9a7437ee5f5
maggieisaacson <misaacson42@gmail.com>
2024-10-15 14:52:00 -0400

    First attempt at HW1 problem 2; numbers are matching, but off by a factor of 10 between MC and other quadrature methods. Will solve when not brain dead. problem 3 file also here

---
+++ Homework1/problem2.m
@@ -0,0 +1,65 @@
+% Integration 
+% Compute the integral from 0 to T of e^(- rho t) u(1 - e^(- lambda t) dt 
+% for T = 100, rho = 0.04, lambda = 0.02, and u(.) = -e ^(-c) using
+% quadrature (midpoint, trapezoid, and simpson rule) and a Monte Carlo.
+% Then compare the performance. 
+
+%% 0. Housekeeping 
+clear variables
+close all 
+
+%% 1. Variable Definitions 
+T = 100 ; 
+rho = 0.04 ; 
+lambda = 0.02 ; 
+
+%% 2. Quadrature Methods 
+% number of intervals n in quadrature method 
+% Match the notation from the notes for clarity 
+n = 1000 ; 
+a = 0 ; 
+b = T ; 
+h = (b - a)/n ; 
+midpoint_integral = 0 ; 
+for j = 1:n
+    a_j = a + (j-1)*h;
+    b_j = a + (j)*h;
+    c_j = (a_j + b_j)/2 ; 
+    % Midpoint integral (sum) 
+    midpoint_integral = midpoint_integral + (b_j-a_j)*f(c_j,rho,lambda) ;
+end 
+
+% Trapezoid integral 
+
+trapezoid_integral = 0 ; 
+for j = 1:n-1 
+    a_j = a + (j-1)*h;
+    b_j = a + (j)*h;
+    trapezoid_integral = trapezoid_integral + (b_j-a_j)/2*(f(a_j,rho,lambda) + f(b_j,rho,lambda)) ; 
+end 
+
+% Simpson's Rule 
+simpsons_integral = 0 ; 
+for j = 1:n 
+    a_j = a + (j-1)*h;
+    b_j = a + (j)*h;
+    c_j = (a_j + b_j)/2 ; 
+    simpsons_integral = simpsons_integral + (b_j - a_j)/6*(f(a_j, rho, lambda) + 4*f(c_j,rho,lambda) + f(b_j, rho, lambda)); 
+end 
+
+%% 3. Monte Carlo 
+rng(926) ;
+mc_integral = 0 ; 
+X = 100*rand(n,1) ;
+f_X = arrayfun(@f,X,rho*ones(n,1),lambda*ones(n,1)) ;
+mc_integral = mean(f_X, "all") ;
+ 
+
+%% 4. Comparison 
+
+
+
+%% Function Definitions 
+function y = f(x,rho,lambda)
+    y = exp(-rho*x)*(-exp(1-exp(-lambda*x)));
+end 
---
+++ Homework1/problem3.m
@@ -0,0 +1,16 @@
+% Optimization: Basic Problem 
+% Use the Newton-Raphson, BFGS, steepest descent, and conjugate descent
+% method to solve the equation 
+%  min_{x,y} 100(y - x^2)^2 + (1 - x)^2 
+
+%% 0. Housekeeping 
+
+%% 1. Newton-Raphson 
+
+%% 2. BFGS 
+
+%% 3. Steepest Descent 
+
+%% 4. Conjugate Descent 
+
+%% Function Appendix 
