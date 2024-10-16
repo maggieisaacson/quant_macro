2a9daaab4728179d8e21f62ac3f1cfadd881be19
maggieisaacson <misaacson42@gmail.com>
2024-10-16 14:40:14 -0400

    Problem 3 code

--- Homework1/problem3.m
+++ Homework1/problem3.m
@@ -7,9 +7,14 @@
 clear variables 
 close all 
 
-% For consistency across methods, maximum iterations allowed will be the
+% For consistency across methods, maximum iterations allowed and the initial guess will be the
 % same and set here 
-maxit = 10000 ; 
+maxit = 50000 ; 
+initial_guess = [1.3; 2] ; 
+
+% Comparison 
+times = zeros(4,1) ;
+iterations = zeros(4,1) ; 
 
 %% 1. Steepest Descent 
 
@@ -20,7 +25,7 @@ tol = 10^-8 ;
 alpha = 0.0001; 
 
 % Ok so it is possibly exactly (1,1) so let's start somewhere else 
-x_y_sd = [0.5, 1.5] ;  
+x_y_sd = initial_guess ;  
 n = 100 ; 
 it = 0 ; 
 
@@ -44,12 +49,13 @@ while n > tol && it < maxit
     it = it + 1 ; 
 end 
 
-toc 
+times(1,1) = toc ;
+iterations(1,1) = it ;
 
 %% 2. Conjugate Descent  
 
 % Initial Guess 
-x_y_cd = [0.5, 1.5] ;  
+x_y_cd = initial_guess ;  
 
 % Initialize the loop 
 n = 100 ; 
@@ -59,7 +65,8 @@ d = 0 ;
 
 tic
 while n > tol && it < maxit
-    % Condition for stopping 
+    % Condition for stopping
+    gradient = grad(x_y_cd) ; 
     n = norm(gradient) ; 
      
     % Calculate new direction 
@@ -77,16 +84,64 @@ while n > tol && it < maxit
     d = dplus ; 
 
     % Update beta for next loop 
-    beta = (gplus*transpose(gplus))/(g*transpose(g)) ;
+    beta = (transpose(gplus)*gplus)/(transpose(g)*g) ;
 end
 
-toc 
+times(2,1) = toc ;  
+iterations(2,1) = it ;
 
 %% 3. Newton-Raphson 
+% The relationship between x and x+1 will be x+1 = x - gradient/hessian 
+% Initial Guess 
+x_y_nr = initial_guess ;  
+
+% Initialize the loop 
+n = 100 ; 
+it = 0 ; 
+
+tic 
+while n > tol && it < maxit 
+    gradient = grad(x_y_nr) ;
+    n = norm(gradient) ; 
+    
+    hess1 = hess(x_y_nr) ;
+    
+    x_y_nr = x_y_nr - inv(hess1)*gradient ; 
+    it = it + 1 ;
+end 
+times(3,1) = toc ; 
+iterations(3,1) = it ;
 
 
 %% 4. BFGS 
 
+% Initial Guess 
+x_y_bfgs = initial_guess ; 
+
+% Loop Setup 
+n = 100 ; 
+it = 0 ;
+H = [1,2;3,4] ; 
+
+tic
+while n > tol && it < maxit 
+    gradient = grad(x_y_bfgs) ; 
+    n = norm(gradient) ; 
+    
+    xn = x_y_bfgs ; 
+    gradn = gradient ; 
+    x_y_bfgs = x_y_bfgs - inv(H)*gradient; 
+    
+    gradient = grad(x_y_bfgs) ; 
+    s = x_y_bfgs - xn ; 
+    y = gradient - gradn ;  
+    
+    H = H + (y*transpose(y))/(transpose(y)*s) - (H*s*transpose(s)*transpose(H))/(transpose(s)*H*s);
+
+    it = it + 1 ; 
+end 
+times(4,1) = toc ; 
+iterations(4,1) = it ;
 
 %% Function Appendix 
 function z = f(pair)
@@ -100,5 +155,14 @@ function z = grad(pair)
     y = pair(2) ; 
     z1 = 200*(y - x^2)*(-2*x) - 2*(1-x) ;
     z2 = 200*(y-x^2) ;
-    z = [z1,z2] ;
+    z = [z1; z2] ;
+end 
+
+function z = hess(pair) 
+    x = pair(1) ;
+    y = pair(2) ; 
+    dxx = -400*y + 1200*x^2 + 2;
+    dxy = -400*x ; 
+    dyy = 200 ;
+    z = [dxx, dxy ; dxy, dyy] ; 
 end 
