ad5daa55fda0a73be7f6169eb433ff0b4ff5aa37
maggieisaacson <misaacson42@gmail.com>
2024-10-27 14:42:12 -0400

    4 done

--- Homework1/problem4.m
+++ Homework1/problem4.m
@@ -24,72 +24,76 @@ n = 3 ;
 % Identical Weights 
 lambda = (1/n)*ones(1,n) ;
 
+% lambda = [1/2, 1/3, 1/6] ; 
+
 % Alpha > 0 
-alpha = 0.5*ones(n,m) ; 
+alpha = 2*ones(n,m) ; 
 
 % Omega < 0
-omega = -0.5*ones(n,m) ; 
+omega = -2*ones(n,m) ; 
 
 % Endowments 
 individual_endow = ones(n,m) ; 
 total_endow = transpose(individual_endow)*ones(n,1) ; 
 
 % Start with a guess 
-x = [0.5,0.5,0.5; 0.5, 0.5, 1.5] ;
-norm_g = 100 ; 
-maxit = 1000 ; 
-it = 0 ; 
-beta = 0 ; 
-alpha_step = 0.01;
-d = 0 ; 
-tolerance = 10^-5 ;  
-   
-%% Loop 
-% Minimize sum 
-while norm_g > tolerance && it < maxit 
+x = [0.5,1,1.5; 1.5,1,0.5] ;
+A = [];
+b = [] ;
+Aeq = [] ; 
+beq = [] ; 
+lb = zeros(n-1,m) ;
 
-    % Gradient written on x as a matrix 
-    x_col = reshape(transpose(x),(n-1)*m,1) ; 
-    grad = gradient_function(x, total_endow,lambda,alpha,omega) ;
+temp = fmincon(@(x) social_planner(x,total_endow,lambda,alpha,omega), x,A,b,Aeq,beq,lb) ; 
+x_last = transpose(total_endow) - ones(1,n-1)*temp ;
 
-    norm_g = norm(grad) ; 
+allocation_3_3 = [temp; x_last] 
 
-    g = gradient_function(x,total_endow,lambda,alpha,omega) ; 
-    g_col = reshape(transpose(g),(n-1)*m,1) ; 
-
-    dplus = -g_col + beta*d ; 
-
-    x_col = x_col + alpha_step.*dplus ; 
-    x = reshape(x_col,n-1,m) ; 
-
-    it = it + 1 ; 
-    gplus = gradient_function(x,total_endow,lambda,alpha,omega); 
-    g_col_plus = reshape(transpose(gplus),(n-1)*m,1) ; 
-    d = dplus ; 
-    
-    beta = (transpose(g_col_plus)*g_col_plus)/(transpose(g_col)*g_col) ; 
-end 
-
-%% 2. Compute Pareto Efficient Allocations (changing parameters) 
-
+% %% 2. Compute Pareto Efficient Allocations (changing parameters) 
+% 
+% % "Hyperparameters" 
+% m = 10 ; 
+% n = 10 ; 
+% 
+% % Identical Weights 
+% lambda = (1/n)*ones(1,n) ;
+% 
+% % Not identical weights
+% % lambda = ((1:n)*ones(n,1))^(-1)*(1:10) ;
+% 
+% % Alpha > 0 
+% alpha = 0.5*ones(n,m) ; 
+% 
+% % Omega < 0
+% omega = -0.5*ones(n,m) ; 
+% 
+% % Endowments 
+% individual_endow = 3*ones(n,m) ; 
+% total_endow = transpose(individual_endow)*ones(n,1) ; 
+% 
+% % Start with a guess 
+% x = 3*ones(n-1,m) ;
+% lb = zeros(n-1,m) ;
+% 
+% social_planner(x,total_endow,lambda,alpha,omega)
+% 
+% temp = fmincon(@(x) social_planner(x,total_endow,lambda,alpha,omega), x,A,b,Aeq,beq,lb) ; 
+% x_last = transpose(total_endow) - ones(1,n-1)*temp ;
+% 
+% endowment_10_10 = [temp; x_last] 
 
 
 %% Function appendix 
-function z = gradient_function(x, endow,lambda,alpha,omega)
-    % Size of x is actually n-1 from above 
+function z = social_planner(x, endow, lambda, alpha, omega)
+     % Size of x is actually n-1 from above 
     [n,~] = size(x) ; 
     
     % Create variables for last person versus all 
-    x_last = transpose(endow) - ones(1,n)*x ; 
-    alpha_1_nminus = alpha(1:n,:) ; 
-    alpha_n = alpha(n+1,:) ; 
-    omega_1_nminus = omega(1:n,:) ; 
-    omega_n = omega(n+1,:) ; 
-    lambda_1_nminus = lambda(1:n) ; 
-    lambda_n = lambda(n+1) ; 
+    x_last = transpose(endow) - ones(1,n)*x ;
+    
+    x_all = [x; x_last] ; 
 
-    impact = lambda_n*alpha_n.*x_last.^omega_n ; 
-    impact = [impact; impact] ;
-
-    z = impact - transpose(lambda_1_nminus).*alpha_1_nminus.*x.^omega_1_nminus ;
-end
+    % Utility 
+    utility = (transpose(lambda).*alpha.*x_all.^omega) ; 
+    z = -1*ones(1,n+1)*utility*ones(n+1,1) ; 
+end 
