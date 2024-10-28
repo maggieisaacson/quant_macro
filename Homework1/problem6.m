e485d9628f4e53a6db06620a02f1fb1519305f81
maggieisaacson <misaacson42@gmail.com>
2024-10-27 21:01:11 -0400

    steady state done

--- Homework1/problem6.m
+++ Homework1/problem6.m
@@ -11,18 +11,23 @@ close all
 % Find the deterministic steady state associated with tau = 0.25 and z = 0 
 
 % Parameters 
-% beta tau z 
-parameters = [0.97, 0.25] ;
+% beta tau z alpha
+parameters = [0.97, 0.25, 0, 0.33] ;
 
 % Initial Guess
-% Investment and Labor 
-i_guess = [1, 0.5] ;
+% Consumption, Investment, Labor, Government, and Capital
+i_guess = [1, 0.5, 1, 1, 1] ;
 
-results = fsolve(@(x) steady_state(parameters,x),i_guess) ; 
+social_planner_results = fsolve(@(x) planner_ss(parameters,x),i_guess) ; 
 
-steady_state(parameters,results) 
+% Full Solution 
+i_guess_full = [1, 1, 1, 1] ; 
+
+competitive_results = fsolve(@(x) comp_ss(parameters,x),i_guess_full) ; 
 
 %% 6.3 Value Function Iteration with Fixed Grid 
+
+
 
 %% 6.4 VFI with Endogenous Grid 
 
@@ -35,25 +40,47 @@ steady_state(parameters,results)
 %% 6.8 Stochastic Grid
 
 %% Function Appendix 
-function euler = steady_state(parameters, initial_guess) 
+function eqn = planner_ss(parameters, initial_guess) 
     
-    % Read in parameters 
+    % Read in parameters (excluding tau)
+    beta = parameters(1) ; 
+    z = parameters(3) ;
+    alpha = parameters(4) ; 
+    
+    % Parameters 
+    c = initial_guess(1) ;
+    inv = initial_guess(2) ;
+    l = initial_guess(3) ;
+    g = initial_guess (4) ;
+    k = initial_guess(5) ; 
+
+    % Need to pull out c, i, g, l, and k
+    eqn(1) = 1/c - 0.2/g ; 
+    eqn(2) = 0.1*k - (1 - 0.05*(inv/inv-1)^2)*inv ; 
+    eqn(3) = c - exp(z)*(1-alpha)*k^alpha*l^(-(1+alpha)) ; 
+    eqn(4) = c + inv + g - exp(z)*k^alpha*l^(1-alpha) ; 
+    eqn(5) = 1/c + beta*(1/c)*alpha*exp(z)*(k/l)^(alpha-1) - 0.9*beta*1/c ; 
+end 
+
+function eqn = comp_ss(parameters,initial_guess)
+     % Read in parameters 
     beta = parameters(1) ; 
     tau = parameters(2) ; 
-
-    inv = initial_guess(1) ;
-    l = initial_guess(2) ;
-
-    k = (10*inv) ; 
-    r = (k/l)^(-0.67) ; 
-    w = (k/l)^(0.33) ; 
-    c = (1 - tau)*w*l + r*k - inv ;
-
-    euler(1) = [18*w*l + 4*r*k - 4*inv]/(5*w*l*(3*w*l + 4*r*k - 4*inv)) - (l/w) ; 
-    euler(2) = (1/c) - (beta*(0.9-r))/c ; 
+    z = parameters(3) ;
+    alpha = parameters(4) ; 
     
-    %((180*inv) + 4 ((10inv)/l)^(-0.67)*10inv - 4inv)/(15*((10*inv)^(0.33)*l^(0.67))^2 + ... 
-       % 20*(10inv/l)^(-0.67)*(10inv)*(10inv/l)^(0.33)*l - 20*inv*(10inv)^0.33*l^0.67) - l*(l/(10*inv))^0.33 ;
-    
-    % euler(2) =  1/( ((1-tau)*(10inv)^0.33*l^0.67) + ((10inv/l)^(-0.67)*10inv) - inv ) - beta*(0.9 - (10inv/l)^(-0.67) )/(((1-tau)*(10inv)^0.33*l^0.67) + (10inv/l)^(-0.67)*10inv - inv) ;  
+    % Parameters 
+    c = initial_guess(1) ;
+    inv = initial_guess(2) ;
+    l = initial_guess(3) ;
+    k = initial_guess(4) ; 
+
+    w = (1 - alpha)*exp(z)*(k/l)^alpha ; 
+    r = alpha*exp(z)*(k/l)^(alpha-1) ; 
+    g = tau*w*l ; 
+
+    eqn(1) = 0.1*k - (1 - 0.05*(inv/inv-1)^2)*inv ; 
+    eqn(2) = (1-tau)*w*l + r*k - c - inv ; 
+    eqn(3) = (1-tau)*(w/l) - c ; 
+    eqn(4) = 1/beta - 0.9 - alpha*exp(z)*(k/l)^(alpha-1) ; 
 end 
